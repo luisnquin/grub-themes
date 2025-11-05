38fe2245e3c5c23cf10bbc1d9e2ed17b2069d389
Luis Quiñones <lpaandres2020@gmail.com>
2025-11-04 20:00:54 -0500

    feat: update fallout grub theme
    I was thinking about how much you hate me

--- nix/pkgs/fallout.nix
+++ nix/pkgs/fallout.nix
@@ -8,8 +8,8 @@ stdenv.mkDerivation rec {
   src = fetchFromGitHub {
     owner = "luisnquin";
     repo = name;
-    rev = "f3b8dafc266363a8d988b0eda7a5e92d450b1bd7";
-    sha256 = "sha256-nahcgnNYl8mZpEcN8UDcqOSrGpoJ4dy3C1v07bXg0eM=";
+    rev = "8271296eb3841a5f8f9e768414eef7b738f570ba";
+    hash = "sha256-fcUqy5n/fE8TwKhf+knUL/mEhBCorAJZd9tHNypwUvQ=";
   };
 
   installPhase = ''
